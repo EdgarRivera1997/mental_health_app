1c891c7e2d38fede05d33200598b08b004575efe
Edwin X. Borrero <37230972+edwinborrero@users.noreply.github.com>
2021-02-26 22:08:40 -0400

    user creation backend

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 export default () => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/MHA', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb://localhost/MHA', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true});
     mongoose.connection
         .once('open', () => console.log('MongoDB running'))
         .on('error', err => console.log(err))
--- backend/server/modules/posts/model.js
+++ backend/server/modules/posts/model.js
@@ -3,12 +3,14 @@ import mongoose, {Schema} from "mongoose";
 const PostSchema = new Schema({
     title: {
        type: String,
-        required: true
+        required: true,
+        minlength: [5, '5 characters long at least']
     },
     body: {
         type: String,
-        required: true
+        required: true,
+        minlength: [10, '10 characters long at least']
     }
-});
+}, { timestamps: true} );
 
 export default mongoose.model('Post', PostSchema);
