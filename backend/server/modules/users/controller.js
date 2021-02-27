177f97914f5c842fe702dabe2e5bad08bf16a478
Edwin X. Borrero <37230972+edwinborrero@users.noreply.github.com>
2021-02-27 18:03:58 -0400

    sign up & sign in backend

--- backend/server/modules/users/controller.js
+++ backend/server/modules/users/controller.js
@@ -1,27 +1,60 @@
 import User from './model.js';
 import Bcrypt from 'bcrypt';
+import passport from 'passport';
 
 export const createUser = async (req, res) => {
-    //const { firstName, lastName, username, email, password, phoneNumber, physicalDirection, age, gender } = req.body;
 
-    const saltPassword = await Bcrypt.genSalt(10)
-    const securePassword = await Bcrypt.hash(req.body.password, saltPassword)
+    User.findOne({ email: req.body.email }, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("Email Already Exists");
+        if (!doc) {
 
-    const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        email: req.body.email,
-        password: securePassword,
-        phoneNumber: req.body.phoneNumber,
-        physicalDirection: req.body.physicalDirection,
-        age: req.body.age,
-        gender: req.body.gender
-    })
+            const saltPassword = await Bcrypt.genSalt(10)
+            const securePassword = await Bcrypt.hash(req.body.password, saltPassword)
 
+            const newUser = new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username,
+                email: req.body.email,
+                password: securePassword,
+                phoneNumber: req.body.phoneNumber,
+                physicalDirection: req.body.physicalDirection,
+                age: req.body.age,
+                gender: req.body.gender
+            })
+
+            try {
+                return res.status(201).json({ user: await newUser.save() });
+            } catch {
+                return res.status(404).json({ error: true, message: 'Error with user'})
+            }
+        }
+    });
+}
+
+export const loginUser = async (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) throw err;
+        if (!user) res.send("No User Exists");
+        else {
+            req.logIn(user, (err) => {
+                if (err) throw err;
+                res.send("Successfully Authenticated");
+                console.log(req.user);
+            });
+        }
+    })(req, res, next);
+}
+
+export const getUser = async (req, res) => {
+    res.send(req.user);  // The req.user stores the entire user that has been authenticated inside of it.
+}
+
+export const getAllUsers = async (req, res) => {
     try {
-        return res.status(201).json({ user: await newUser.save() });
+        return res.status(200).json({ post : await User.find({} )});
     } catch {
-        return res.status(404 ).json({ error: true, message: 'Error with user'})
+        return res.status(404).json({ error: true, message: 'Error with User'});
     }
 }
--- backend/server/modules/users/routes.js
+++ backend/server/modules/users/routes.js
@@ -3,6 +3,9 @@ import * as UserController from './controller.js';
 
 const routes = new Router();
 
-routes.post('/user', UserController.createUser);
+routes.post('/signup', UserController.createUser);
+routes.post('/signin', UserController.loginUser);
+routes.get('/user', UserController.getUser);
+routes.get('/allUsers', UserController.getAllUsers);
 
 export default routes;
